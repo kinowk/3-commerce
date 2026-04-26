3427778c06ab5deacbc5d4cb6751232a868f5d94
박권익 <kinowk@naver.com>
2026-04-26 14:38:06 +0900

    refactor: "X-USER-ID" -> ApiHeader 사용

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
@@ -3,6 +3,7 @@ package com.loopers.interfaces.api.user;
 import com.loopers.application.user.UserFacade;
 import com.loopers.application.user.UserInput;
 import com.loopers.application.user.UserOutput;
+import com.loopers.interfaces.api.ApiHeader;
 import com.loopers.interfaces.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +30,7 @@ public class UserV1Controller {
     }
 
     @GetMapping("/me")
-    public ApiResponse<UserResponse.GetUser> getCurrentUser(@RequestHeader("X-USER-ID") String loginId) {
+    public ApiResponse<UserResponse.GetUser> getCurrentUser(@RequestHeader(ApiHeader.X_USER_ID) String loginId) {
         UserOutput.GetUser output = userFacade.getUser(loginId);
         UserResponse.GetUser response = UserResponse.GetUser.from(output);
         return ApiResponse.success(response);
