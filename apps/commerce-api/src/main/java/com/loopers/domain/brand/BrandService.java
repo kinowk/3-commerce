9ca07a87ffa018ed178ecd3df73e86a26772ac9c
박권익 <kinowk@naver.com>
2026-04-26 21:26:24 +0900

    fix: Transactional 어노테이션 누락 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/brand/BrandService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/BrandService.java
@@ -4,9 +4,11 @@ import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BrandService {
 
     private final BrandRepository brandRepository;
